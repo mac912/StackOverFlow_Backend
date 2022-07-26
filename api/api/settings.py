e9e74bb004cd98139d870b6919c14e992a0641f0
manish saini <manishdwarkas912@gmail.com>
2022-07-26 14:42:45 +0530

    Update settings.py

--- api/api/settings.py
+++ api/api/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-2u&%x6)jt@uh#l6y$1za@056v=&obsr5#e@gromv!0+-t74!#^
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
