a57be4e5501f20992188a79b11b502fe0b8c534c
manish saini <manishdwarkas912@gmail.com>
2022-07-26 14:27:09 +0530

    Update settings.py

--- api/api/settings.py
+++ api/api/settings.py
@@ -93,12 +93,23 @@ WSGI_APPLICATION = 'api.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+     'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'mydb',
+          'USER': 'manish', 
+          'PASSWORD': 'manish', 
+          'HOST': 'dbos', 
+          'PORT': '5432',
     }
 }
+
 
 
 # Password validation
