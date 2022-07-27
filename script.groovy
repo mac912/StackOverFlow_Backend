050a5a83484468a66240bd48eeef2f0a34a222a2
manish saini <manishdwarkas912@gmail.com>
2022-07-27 13:13:09 +0530

    Update script.groovy

--- script.groovy
+++ script.groovy
@@ -27,7 +27,7 @@ job("my-job2") {
  
   command = """
     project_folder=\$(cat /var/lib/jenkins/workspace/my-job1/proname)
-    docker build-arg project_folder -t django_os_server /var/lib/jenkins/workspace/my-job1 --no-cache
+    docker build --build-arg project_folder=$project_folder -t django_os_server /var/lib/jenkins/workspace/my-job1 --no-cache
     docker-compose up &
 """
  
