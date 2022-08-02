45af6ce7b4bc051d163be88337630454780e90b8
manish saini <manishdwarkas912@gmail.com>
2022-08-02 10:50:05 +0530

    Update script.groovy

--- script.groovy
+++ script.groovy
@@ -63,13 +63,13 @@ job("my-job3") {
           attachBuildLog(true)
           subject('Build successfull')
           content('The build was successful and deployment was done.')
-          recipientList('vinithamoorthy22@gmail.com')
+          recipientList('products.notifications@ganitinc.com')
         }
         failure{
           attachBuildLog(true)
           subject('Failed build')
           content('The build was failed')
-          recipientList('vinithamoorthy22@gmail.com')
+          recipientList('products.notifications@ganitinc.com')
         }
       }
     }
